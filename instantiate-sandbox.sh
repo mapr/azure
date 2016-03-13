9e8c63ed0d83dccceeb2685dc8fbeaf09daf5435
dbtucker <dbtucker@cal.berkeley.edu>
2016-03-13 10:16:49 -0700

    Fixed to allow patching.

--- instantiate-sandbox.sh
+++ instantiate-sandbox.sh
@@ -6,13 +6,34 @@
 # NOTE: Optional $1 argument will support downloading a trial license
 # for use during installation.   
 #
-# FUTURE DESIGN :
+# PATCH DESIGN :
 #	This is the right place to "patch" the AMI with updated scripts/etc.
 #	The Azure Template process can be used to upload files into the 
-#	same directory from which this script is executed ... so it
-#	could be modified to check for the presense of "patches" and move
+#	same directory from which this script is executed ... so we 
+#	have modified the logic to check for the presense of "patches" and move
 #	them into place within /home/mapr before starting the deployment.
 #
+
+THIS=`readlink -f $0`
+BINDIR=`dirname $THIS`
+
+
+MAPR_USER=${MAPR_USER:-mapr}
+MAPR_USER_DIR=`eval "echo ~${MAPR_USER}"`
+MAPR_USER_DIR=${MAPR_USER_DIR:-/home/mapr}
+
+patch_ami() {
+	AMI_SBIN=${MAPR_USER_DIR}/sbin
+	for f in $(cd ${AMI_SBIN}; ls) ; do
+		[ ! -f $BINDIR/$f ] && continue
+
+		cp -p ${AMI_SBIN}/$f ${AMI_SBIN}/${f}.ami
+		cp ${BINDIR}/$f ${AMI_SBIN}/$f
+	done
+}
+
+patch_ami
+
 
 if [ -n "$1" ] ; then
 	LIC_TYPE=$1
